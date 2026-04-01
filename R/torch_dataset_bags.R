45bf674fa566768febaa5a2549866fbdee20666a
Julien.Prados <julien.prados@unige.ch>
2026-04-01 15:47:10 +0200

    Change bag inerface for a more generic one

--- R/torch_dataset_bags.R
+++ R/torch_dataset_bags.R
@@ -1,10 +1,10 @@
 
 
 
-#' @title bag_sampling_classification_dataset
+#' @title bag_sampling_dataset
 #' @description Given labelled bags of instances, randomly generate new bags bag_size by random sampling random sample instances
 #' @param x a generic 2D matrix object, with elements in rows and features as column
-#' @param sample_ids a factor whose length match nrow(x) defining bag of elements
+#' @param bags a list of integer vectors referencing rows of x
 #' @param y a vector of target values for each bag, so the length must match nlevels(sample_ids)
 #' @param nbag number of random bags to generate in the dataset
 #' @param bag_size size of the bags to generate
@@ -14,30 +14,31 @@
 #' @param balanced a logical, if TRUE will generate the same number of bag for each target value
 #' @import torch
 #' @importFrom tibble tibble rowid_to_column
-#' @importFrom dplyr mutate inner_join select slice_sample slice_head arrange group_by reframe slice
+#' @importFrom dplyr mutate inner_join select slice_sample slice_head group_by ungroup slice summarize n
 #' @importFrom purrr map
+#' @importFrom tidyr unnest
 #' @export
 #' @examples
-#' bags <- bag_sampling_classification_dataset(
+#' bags <- bag_sampling_dataset(
 #'   matrix((1:1000-1)%%100+1,100,10),
-#'   gl(20,5),
+#'   split(seq(100),gl(20,5)),
 #'   gl(2,10),
 #'   nbag = 7L,bag_size=5L,balanced=TRUE
 #' )[1:3]
-bag_sampling_classification_dataset <- torch::dataset(
+bag_sampling_dataset <- torch::dataset(
   name = "bag_sampling_dataset",
 
-  initialize = function(x,sample_ids,y,nbag=1000L,bag_size=100L,seed=1234L,replace=TRUE,post_process=identity,balanced=TRUE) {
-    sample_ids <- as.factor(sample_ids)
-    stopifnot(identical(length(sample_ids),nrow(x)))
-    stopifnot(identical(length(y),nlevels(sample_ids)))
+  initialize = function(x,bags,y,nbag=1000L,bag_size=100L,seed=1234L,replace=TRUE,post_process=identity,balanced=TRUE) {
+    stopifnot(identical(length(y),length(bags)))
+    stopifnot("some integers in bags are out of range" = max(unlist(bags))<=nrow(x))
+    stopifnot("some integers in bags are out of range" = min(unlist(bags))>=1L)
     stopifnot(all(!is.na(y)))
     self$post_process <- post_process
     self$x <- x
-    self$samples <- tibble(
-      sample_id = levels(sample_ids),
+    self$input_bags <- tibble(
+      input_bag_idx = seq_along(bags),
       y = y,
-      cells = split(seq(nrow(x)),sample_ids)
+      elements = bags
     )
 
     #-#-#-#-#-#-#-#-#
@@ -47,29 +48,27 @@ bag_sampling_classification_dataset <- torch::dataset(
     # First assign one sample to each bag
     set.seed(seed)
     if (balanced) {
-      self$bags <- self$samples |>
-        select(sample_id,y) |>
-        slice_sample(prop=1) |> # randomize
+      self$bags <- self$input_bags |>
+        select(input_bag_idx,y) |>
         group_by(y) |>
-        reframe(
-          i = seq(nbag),
-          sample_id = rep(sample_id,length.out=nbag)
-        ) |>
-        arrange(i,y) |>
+        summarize(input_bag_idx = list(input_bag_idx)) |>
+        ungroup() |>
+        mutate(input_bag_idx = map(input_bag_idx,~sample(.,nbag %/% n() + 1L,replace=replace))) |>
+        unnest(input_bag_idx) |>
+        slice_sample(prop=1) |>
         slice_head(n=nbag) |>
-        select(!c(i,y)) |>
+        select(!y) |>
         rowid_to_column("bag_id")
     } else {
       self$bags <- tibble(bag_id = seq(nbag)) %>%
         # randomly select a sample for each bag to draw from it
-        mutate(sample_id = sample(rep(self$samples$sample_id,length.out = n())))
+        mutate(input_bag_idx = sample(rep(self$input_bags$input_bag_idx,length.out = n())))
     }
 
-    # Then randomly select cells from selected sample
+    # Then randomly select elements from selected sample
     self$bags <- self$bags %>%
-      inner_join(self$samples,by="sample_id",relationship="many-to-many") %>%
-      mutate(cells = map(cells,sample,size=bag_size,replace=replace)) %>%
-      mutate(cells = do.call(rbind,cells))
+      inner_join(self$input_bags,by="input_bag_idx",relationship="many-to-many") %>%
+      mutate(elements = map(elements,sample,size=bag_size,replace=replace))
   },
   .length = function() {
     nrow(self$bags)
@@ -78,9 +77,10 @@ bag_sampling_classification_dataset <- torch::dataset(
     B <- dplyr::slice(self$bags,index)
     list(
       x = local({
-        x <- self$x[B$cells,] |> as.matrix()
-        dim(x) <- c(dim(B$cells),ncol(self$x))
-        torch_tensor(x)
+        x <- self$x[unlist(B$elements,use.names = FALSE),] |>
+          as.matrix() |>
+          torch_tensor() |>
+          torch_reshape(c(nrow(B),-1,ncol(self$x)))
       }),
       y = torch_tensor(B$y)
     ) |> self$post_process()
